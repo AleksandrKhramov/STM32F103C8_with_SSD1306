3593ea6077d6b20e8b06258fa873689a47d9ed18
AleksandrKhramov <sanya.hramov@ya.ru>
2019-12-09 14:03:29 +0300

    declarations to SSD1306 header added

---
+++ Workspace/SSD1306/inc/SSD1306.h
@@ -0,0 +1,8 @@
+void WriteCmd1306(unsigned char cmd);
+void WriteData1306(unsigned char dat);
+void SetAddr1306(unsigned char page,unsigned char lCol,unsigned char hCol);
+void HomeAddr1306(void);
+//void Full_on(void);
+void InitSSD1306(void);
+
+
